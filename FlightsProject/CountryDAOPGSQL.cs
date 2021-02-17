5330026e8fac6356d4c0c02a67502dc5587f31af
User <User@DESKTOP-0BJFLEG>
2021-02-17 13:47:08 +0200

    Add project files.

---
+++ FlightsProject/CountryDAOPGSQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FlightsProject
+{
+    public class CountryDAOPGSQL : ICountryDAO
+    {
+        void IBasicDb<Country>.Add(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        Country IBasicDb<Country>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<Country> IBasicDb<Country>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBasicDb<Country>.Remove(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBasicDb<Country>.Update(Country country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlightsProject/IBasicDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightsProject
+{
+    interface IBasicDb<T> where T : IPoco
+    {
+        T Get(int id);
+        IList<T> GetAll();
+        void Add(T t);
+        void Remove(T t);
+        void Update(T t);
+    }
+}
