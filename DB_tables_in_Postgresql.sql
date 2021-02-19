c4bf8cf30c3b6d5ed82624c76d7ce5a91c69b575
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-19 20:31:18 +0200

    Update DB_tables_in_Postgresql.sql

--- DB_tables_in_Postgresql.sql
+++ DB_tables_in_Postgresql.sql
@@ -1,15 +1,15 @@
 CREATE TABLE Countries (
-    Id SERIAL PRIMARY KEY,
+    	Id SERIAL PRIMARY KEY,
 	Name TEXT UNIQUE
 	);
 	
 CREATE TABLE User_Roles (
-    Id SERIAL PRIMARY KEY,
+    	Id SERIAL PRIMARY KEY,
 	Role_Name TEXT UNIQUE
 	);
 
 CREATE TABLE Users (
-    Id BIGSERIAL PRIMARY KEY,
+    	Id BIGSERIAL PRIMARY KEY,
 	Username TEXT UNIQUE,
 	Password  TEXT,
 	Email  BIGINT UNIQUE,
@@ -17,14 +17,14 @@ CREATE TABLE Users (
 	);
 	
 CREATE TABLE Airline_Companies(
-   Id BIGSERIAL PRIMARY KEY,
-   Name TEXT UNIQUE ,
-   CountryId INT references Countries(Id),
-   User_Id BIGINT UNIQUE references User_Roles(Id)
+	Id BIGSERIAL PRIMARY KEY,
+	Name TEXT UNIQUE ,
+	CountryId INT references Countries(Id),
+	User_Id BIGINT UNIQUE references User_Roles(Id)
 	);
 
 CREATE TABLE Administrators (
-    Id SERIAL PRIMARY KEY,
+    	Id SERIAL PRIMARY KEY,
 	First_Name TEXT,
 	Last_Name TEXT,
 	Level int,
@@ -32,7 +32,7 @@ CREATE TABLE Administrators (
 	);
 	
 CREATE TABLE Customers(
-    Id BIGSERIAL PRIMARY KEY,
+    	Id BIGSERIAL PRIMARY KEY,
 	First_Name TEXT,
 	Last_Name TEXT,
 	Address TEXT,
@@ -42,7 +42,7 @@ CREATE TABLE Customers(
 	);
 	
 CREATE TABLE Flights(
-    Id BIGSERIAL PRIMARY KEY,
+    	Id BIGSERIAL PRIMARY KEY,
 	Airline_Company_Id BIGINT references Airline_Companies(Id),
 	Origin_Country_Id INT references Countries(Id),
 	Destination_Country_Id INT references Countries(Id),
@@ -52,7 +52,7 @@ CREATE TABLE Flights(
 	);
 	
 CREATE TABLE Tickets(
-    Id BIGSERIAL PRIMARY KEY,
+    	Id BIGSERIAL PRIMARY KEY,
 	Id_Flight BIGINT UNIQUE references Flights(Id),
 	Id_Customer BIGINT UNIQUE references Customers(Id)
 	)
